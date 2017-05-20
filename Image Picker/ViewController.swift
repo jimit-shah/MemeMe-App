8bbfdab800b99d5fb0173950da77f0bf39fa1be9
jshah77 <jimit.shah@thefreeosk.com>
2017-05-20 15:26:22 -0700

    Initial Commit

---
+++ Image Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image Picker
+//
+//  Created by Jimit Shah on 5/20/17.
+//  Copyright © 2017 Jimit Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
